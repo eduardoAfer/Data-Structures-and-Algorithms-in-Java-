8ef0986be634a72bb128886de1d2c17971916dc5
Eduardo Ferreira <80621444+eduardoAfer@users.noreply.github.com>
2022-03-12 15:56:54 +0000

    init

---
+++ week2/DAA005/DAA005.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class DAA005 {
+
+   public static void energySet(int set[], int a, int b){ 
+        if( a == 1) FastPrint.out.println(set[b -1]);
+        else FastPrint.out.println(set[b -1 ] - set[a - 2]);
+    }
+
+    public static void main(String args[]){
+        FastScanner in = new FastScanner(System.in);
+
+        int ctrl = in.nextInt();
+        int collection[] = new int[ctrl];
+        collection[0] = 0; 
+
+        for(int i = 0; i < ctrl; i++){
+            if(i == 0) collection[i] = in.nextInt();
+            else collection[i] = collection[i -1] + in.nextInt();
+
+        }
+        
+        ctrl = in.nextInt();
+        for(int i = 0; i < ctrl; i++){
+           energySet(collection, in.nextInt(), in.nextInt());
+        }
+        FastPrint.out.close();
+    }
+    
+    
+}
---
+++ week2/DAA005/FastPrint.java
@@ -0,0 +1,6 @@
+
+import java.io.*;
+
+class FastPrint {
+    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+}
